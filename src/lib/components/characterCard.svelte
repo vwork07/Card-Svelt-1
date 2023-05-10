cf1c5e724d3f1920d1a58144ac704bf1ecb765af
vwork07 <132792713+vwork07@users.noreply.github.com>
2023-05-10 15:13:19 -0400

    2 files modified, 1 added, 6 deleted

---
+++ src/lib/components/characterCard.svelte
@@ -0,0 +1,10 @@
+<script>
+<div class="bg-white rounded-lg overflow-hidden shadow-md">
+  <img class="w-full h-48 object-cover" src="https://picsum.photos/400/300" alt="Card image">
+  <div class="p-4">
+    <h3 class="text-xl font-medium mb-2">Card Title</h3>
+    <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">Read more</a>
+  </div>
+</div>
+</script>
