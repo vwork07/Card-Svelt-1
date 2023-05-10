c75b485451f80cf23885bee5157dca78271d71cd
vwork07 <132792713+vwork07@users.noreply.github.com>
2023-05-10 16:35:11 -0400

    1 file modified, 1 added, 1 deleted

---
+++ src/lib/components/CharacterCard.svelte
@@ -0,0 +1,21 @@
+<script>
+export let characterIndex;
+export let characterModel;
+export let characterName;
+export let altCharacterName;
+export let characterDescription;
+export let atkStat;
+export let defStat;
+export let hpStat;
+</script>
+
+<div class="bg-white rounded-lg overflow-hidden shadow-md">
+  <img class="w-full h-64 object-cover" src="{characterModel}" alt="altCharacterName">
+  <div class="p-4">
+    <h3 class="text-xl font-medium mb-2">{characterName}</h3>
+    <p class="text-gray-700 text-base">{characterDescription}</p>
+      <p class="text-gray-700 text-base">HP:{hpStat}</p>
+    <p class="text-gray-700 text-base">Atk: {atkStat}</p>
+    <p class="text-gray-700 text-base">Def: {defStat}</p>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,63 @@
 <script>
-	import characterCard from '$lib/components/characterCard.svelte';
+	import CharacterCard from '$lib/components/CharacterCard.svelte';
+let temp1characterIndex = 2;
+let temp1characterModel = "https://media.tenor.com/aIr8iKZ3UdIAAAAi/hasher-sticker.gif";
+let temp1characterName = "Bengal Destroyer";
+let temp1altCharacterName = "waka man";
+let temp1characterDescription = "man the man man";
+let temp1atkStat= 30;
+let temp1defStat= 50;
+let temp1hpStat = 150;
+
+let tempcharacterIndex = 1;
+let tempcharacterModel = "https://media.tenor.com/wrMDu29fA-YAAAAi/hasher-happy-sticker.gif";
+let tempcharacterName = "bone papichu ";
+let tempaltCharacterName = "bone boy";
+let tempcharacterDescription = "the greatst of bone keepers";
+let tempatkStat= 50;
+let tempdefStat= 30;
+let temphpStat = 150;
+
+function attack() {
+
+}
+
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
+
+<div class="grid grid-cols-2 gap-4 mt-20 mx-5">
+<div class="m-2">
+<CharacterCard 
+ characterIndex = {tempcharacterIndex}
+ characterName = {tempcharacterName}
+ altCharacterName = {tempaltCharacterName}
+ characterDescription = {tempcharacterDescription}
+ characterModel = {tempcharacterModel}
+ atkStat = {tempatkStat}
+ defStat = {tempdefStat}
+ hpStat = {temphpStat}/>
+</div>
+<div class="m-2">
+<CharacterCard
+characterIndex = {temp1characterIndex}
+ characterName = {temp1characterName}
+ altCharacterName = {temp1altCharacterName}
+ characterDescription = {temp1characterDescription}
+ characterModel = {temp1characterModel}
+ atkStat = {temp1atkStat}
+ defStat = {temp1defStat}
+ hpStat = {temp1hpStat}/>
+
+</div>
+
+</div>
+<div class="text-center mt-20">
+<button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600 hover:shadow-lg hover:scale-110" on:click={attack} > Attack</button>
+</div>
 
 
 <style>
